25a7358854571db0ed47cc75dde56305aac1fe2c
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-22 22:08:25 -0300

    implement enhance with clearbit after create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :enhance_clearbit
+
+  private
+
+  def enhance_clearbit
+    EnhanceClearbitJob.perform_later(self.id)
+  end
 end
