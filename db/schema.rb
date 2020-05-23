fcbc6dbf28f0b9a3e6350f75bc47b170295a8614
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-22 22:08:05 -0300

    add data to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_002251) do
+ActiveRecord::Schema.define(version: 2020_05_23_024612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2020_05_23_002251) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
+    t.string "full_name"
+    t.string "location"
+    t.string "avatar_url"
+    t.string "github_handle"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
